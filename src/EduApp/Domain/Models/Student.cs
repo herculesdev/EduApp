4deae0223f42c6b80ab01f0feb068f6c81b5c96d
Hércules Moreira <herculesmoreira22@gmail.com>
2021-11-08 18:43:52 -0300

    feat: estrutura inicial

---
+++ src/EduApp/Domain/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string RA { get; set; }
+        public string CPF { get; set; }
+
+        public Student(long id, string name, string email, string ra, string cpf)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            RA = ra;
+            CPF = cpf;
+        }
+    }
+}
